c71d0e311ac9450ea8a7f880c49bdac1217f5142
Martin <martin.topchyan@gmail.com>
2018-11-26 16:08:23 +0400

    updated config.yml

--- src/main/java/edu/cognaize/cirlceciapidemo/service/CircleCiApiService.java
+++ src/main/java/edu/cognaize/cirlceciapidemo/service/CircleCiApiService.java
@@ -1,17 +1,16 @@
 package edu.cognaize.cirlceciapidemo.service;
 
-//import edu.cognaize.circleci.model.CircleCiModel;
-//import edu.cognaize.circleci.service.CircleCiService;
+import edu.cognaize.circleci.model.CircleCiModel;
+import edu.cognaize.circleci.service.CircleCiService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CircleCiApiService {
 
-//  private CircleCiService circleCiService = new CircleCiService();
-//  private CircleCiModel circleCiModel = new CircleCiModel("Martin Topchyan!!!");
+  private CircleCiService circleCiService = new CircleCiService();
+  private CircleCiModel circleCiModel = new CircleCiModel("Martin Topchyan!!!");
 
   public void sayHello() {
-//    circleCiService.sayHello(circleCiModel);
-    System.out.println("Hello MArtin Topchyan");
+    circleCiService.sayHello(circleCiModel);
   }
 }
