324305878e456542bad4b0aeff887ea09918679b
Martin <martin.topchyan@gmail.com>
2018-11-26 15:54:06 +0400

    removed lib dependency

--- src/main/java/edu/cognaize/cirlceciapidemo/service/CircleCiApiService.java
+++ src/main/java/edu/cognaize/cirlceciapidemo/service/CircleCiApiService.java
@@ -1,16 +1,17 @@
 package edu.cognaize.cirlceciapidemo.service;
 
-import edu.cognaize.circleci.model.CircleCiModel;
-import edu.cognaize.circleci.service.CircleCiService;
+//import edu.cognaize.circleci.model.CircleCiModel;
+//import edu.cognaize.circleci.service.CircleCiService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CircleCiApiService {
 
-  private CircleCiService circleCiService = new CircleCiService();
-  private CircleCiModel circleCiModel = new CircleCiModel("Martin Topchyan!!!");
+//  private CircleCiService circleCiService = new CircleCiService();
+//  private CircleCiModel circleCiModel = new CircleCiModel("Martin Topchyan!!!");
 
   public void sayHello() {
-    circleCiService.sayHello(circleCiModel);
+//    circleCiService.sayHello(circleCiModel);
+    System.out.println("Hello MArtin Topchyan");
   }
 }
